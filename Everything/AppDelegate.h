7b2ec6c9a784a35f63c8ac8147d9b7a7d16f9057
AjaxXu <xu_soft39211215@163.com>
2016-07-07 23:19:03 +0800

    Initial Commit

---
+++ Everything/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Everything
+//
+//  Created by Louis on 16/7/7.
+//  Copyright © 2016年 Louis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Everything/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Everything
+//
+//  Created by Louis on 16/7/7.
+//  Copyright © 2016年 Louis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
